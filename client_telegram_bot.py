bfc717a3a6518ecaab012bf066e6e52987fa8e01
Nixon Thuo <thuonixon808@gmail.com>
2018-05-25 21:34:47 +0300

    Friday 5_25_2018

--- client_telegram_bot.py
+++ client_telegram_bot.py
@@ -34,7 +34,8 @@ global user_logs_json
 global config_settings
 config_settings = {
     'welome_message': 'Hello $username! Welcome to $title',
-    'spam_words': []
+    'spam_words': [],
+    'bot_admins': [268241452]
 }
 print(type(config_settings))
 print(config_settings)
@@ -63,6 +64,13 @@ with open('users_activity.json', 'r') as infile:
     print(user_logs_json)
 
 
+def check_if_bot_admin(user_id):
+    if user_id in config_settings['bot_admins']:
+        return True
+    else:
+        return False
+
+
 # Welcome a user to the chat
 def welcome(bot, update):
     """ Welcomes a user to the chat """
@@ -88,23 +96,24 @@ def welcome(bot, update):
 
 
 def set_welcome_message(bot, update, args):
-    global config_settings
-    """
-    sets the welcome message
-    """
-    print(args)
-    new_message = " ".join(args)
-    if new_message:
-        config_settings["welome_message"] = new_message
-        with open('config_file.json', 'w') as infile:
-            infile.write(json.dumps(config_settings, indent=4))
-        bot.send_message(
-            chat_id=update.message.chat_id,
-            text="new welcome message succesfully set!")
-    else:
-        bot.send_message(
-            chat_id=update.message.chat_id,
-            text="Cannot set empty welcome message!")
+    if check_if_bot_admin(update.message.from_user.id):
+        global config_settings
+        """
+        sets the welcome message
+        """
+        print(args)
+        new_message = " ".join(args)
+        if new_message:
+            config_settings["welome_message"] = new_message
+            with open('config_file.json', 'w') as infile:
+                infile.write(json.dumps(config_settings, indent=4))
+            bot.send_message(
+                chat_id=update.message.chat_id,
+                text="new welcome message succesfully set!")
+        else:
+            bot.send_message(
+                chat_id=update.message.chat_id,
+                text="Cannot set empty welcome message!")
 
 
 def add_spam_word(bot, update, args):
@@ -112,19 +121,20 @@ def add_spam_word(bot, update, args):
     Add new spam word to dictionary
     """
     global config_settings
-    new_spam_word = " ".join(args)
-    if new_spam_word:
-        spam_list = config_settings["spam_words"]
-        spam_list.append(new_spam_word)
-        config_settings["spam_words"] = spam_list
-        with open('config_file.json', 'w') as infile:
-            infile.write(json.dumps(config_settings, indent=4))
-        bot.send_message(
-            chat_id=update.message.chat_id,
-            text="New spam word succesfully set!")
-    else:
-        bot.send_message(
-            chat_id=update.message.chat_id, text="Cannot add empty spam word!")
+    if check_if_bot_admin(update.message.from_user.id):
+        new_spam_word = " ".join(args)
+        if new_spam_word:
+            spam_list = config_settings["spam_words"]
+            spam_list.append(new_spam_word)
+            config_settings["spam_words"] = spam_list
+            with open('config_file.json', 'w') as infile:
+                infile.write(json.dumps(config_settings, indent=4))
+            bot.send_message(
+                chat_id=update.message.chat_id,
+                text="New spam word succesfully set!")
+        else:
+            bot.send_message(
+                chat_id=update.message.chat_id, text="Cannot add empty spam word!")
 
 
 def remove_spam_word(bot, update, args):
@@ -132,42 +142,45 @@ def remove_spam_word(bot, update, args):
     Remove spam word from dictionary
     """
     global config_settings
-    spam_word = " ".join(args)
-    if spam_word:
-        spam_list = config_settings["spam_words"]
-        spam_list.remove(spam_word)
-        config_settings["spam_words"] = spam_list
-        with open('config_file.json', 'w') as infile:
-            infile.write(json.dumps(config_settings, indent=4))
-        bot.send_message(
-            chat_id=update.message.chat_id,
-            text="Spam word removed from dictionary!")
-    else:
-        bot.send_message(
-            chat_id=update.message.chat_id,
-            text="Cannot remove empty spam word!")
+    if check_if_bot_admin(update.message.from_user.id):
+        spam_word = " ".join(args)
+        if spam_word:
+            spam_list = config_settings["spam_words"]
+            spam_list.remove(spam_word)
+            config_settings["spam_words"] = spam_list
+            with open('config_file.json', 'w') as infile:
+                infile.write(json.dumps(config_settings, indent=4))
+            bot.send_message(
+                chat_id=update.message.chat_id,
+                text="Spam word removed from dictionary!")
+        else:
+            bot.send_message(
+                chat_id=update.message.chat_id,
+                text="Cannot remove empty spam word!")
 
 
 def list_spam_words(bot, update):
-    global config_settings
-    spam_list = config_settings["spam_words"]
-    spam_string = " ".join(spam_list)
-    bot.send_message(chat_id=update.message.chat_id, text=spam_string)
+    if check_if_bot_admin(update.message.from_user.id):
+        global config_settings
+        spam_list = config_settings["spam_words"]
+        spam_string = " ".join(spam_list)
+        bot.send_message(chat_id=update.message.chat_id, text=spam_string)
 
 
 def help_menu(bot, update):
     """
     Displays the help menu
     """
-    menu_message = """ BOT HELP MENU:
-    /addspam <spamword> - This command helps to add a new spam word into the bots memory
-    /setwelcome <welcomemessage> - This command allows you to customize the welcome message. Your message should be like this 'Hello $username! Welcome to $title'
-    /mostactive - This command will give you the list of most active users in the group
-    /set <seconds> <message> - This command will allows you to set a message to be displayed after the seconds set
-    /listspamwords - shows the words that have been added to the spam dictionary
-    /delspam <spamword> - deletes a spam word from the spam dictionary
-    """
-    bot.send_message(chat_id=update.message.chat_id, text=menu_message)
+    if check_if_bot_admin(update.message.from_user.id):
+        menu_message = """ BOT HELP MENU:
+        /addspam <spamword> - This command helps to add a new spam word into the bots memory
+        /setwelcome <welcomemessage> - This command allows you to customize the welcome message. Your message should be like this 'Hello $username! Welcome to $title'
+        /mostactive - This command will give you the list of most active users in the group
+        /set <seconds> <message> - This command will allows you to set a message to be displayed after the seconds set
+        /listspamwords - shows the words that have been added to the spam dictionary
+        /delspam <spamword> - deletes a spam word from the spam dictionary
+        """
+        bot.send_message(chat_id=update.message.chat_id, text=menu_message)
 
 
 def empty_message(bot, update):
@@ -236,24 +249,25 @@ def check_spam(bot, update):
 def check_active_users(bot, update):
     # get active users in a group
     global user_logs_json
-    print(user_logs_json)
-    users_message = 'no active users yet'
-    with open('users_activity.json', 'r') as infile:
-        print(infile)
-        if user_logs_json:
-            user_logs_json = json.load(infile)
-            sorted_users = Counter(user_logs_json)
-            top_five = sorted_users.most_common(5)
-            print(top_five)
-            users_message = ''
-            for user_id in top_five:
-                print(user_id[0])
-                users_message += str(
-                    bot.getChatMember(update.message.chat_id, user_id[0])
-                    .user.first_name) + "  " + str(
-                        bot.getChatMember(update.message.chat_id,
-                                          user_id[0]).user.last_name) + '\n'
-    bot.send_message(chat_id=update.message.chat_id, text=users_message)
+    if check_if_bot_admin(update.message.from_user.id):
+        print(user_logs_json)
+        users_message = 'no active users yet'
+        with open('users_activity.json', 'r') as infile:
+            print(infile)
+            if user_logs_json:
+                user_logs_json = json.load(infile)
+                sorted_users = Counter(user_logs_json)
+                top_five = sorted_users.most_common(5)
+                print(top_five)
+                users_message = ''
+                for user_id in top_five:
+                    print(user_id[0])
+                    users_message += str(
+                        bot.getChatMember(update.message.chat_id, user_id[0])
+                        .user.first_name) + "  " + str(
+                            bot.getChatMember(update.message.chat_id,
+                                              user_id[0]).user.last_name) + '\n'
+        bot.send_message(chat_id=update.message.chat_id, text=users_message)
 
 
 def timed_messages(bot, job):
@@ -264,42 +278,44 @@ def timed_messages(bot, job):
 def set_timer(bot, update, args, job_queue, chat_data):
     global message_to_send
     """Add a job to the queue."""
-    chat_id = update.message.chat_id
-    try:
-        # args[0] should contain the time for the timer in seconds
-        due = int(args[0])
-        message_to_send = ' '.join(args[1:])
-        print(message_to_send)
-        if due < 0:
-            update.message.reply_text('Sorry we can not go back to past!')
-            return
+    if check_if_bot_admin(update.message.from_user.id):
+        chat_id = update.message.chat_id
+        try:
+            # args[0] should contain the time for the timer in seconds
+            due = int(args[0])
+            message_to_send = ' '.join(args[1:])
+            print(message_to_send)
+            if due < 0:
+                update.message.reply_text('Sorry we can not go back to past!')
+                return
 
-        # Add job to queue
-        job = job_queue.run_once(timed_messages, due, context=chat_id)
-        chat_data['job'] = job
+            # Add job to queue
+            job = job_queue.run_once(timed_messages, due, context=chat_id)
+            chat_data['job'] = job
 
-        update.message.reply_text('Timed message successfully set!')
+            update.message.reply_text('Timed message successfully set!')
 
-    except (IndexError, ValueError):
-        update.message.reply_text('Usage: /set <seconds>')
+        except (IndexError, ValueError):
+            update.message.reply_text('Usage: /set <seconds>')
 
 
 def unset(bot, update, chat_data):
     """Remove the job if the user changed their mind."""
-    if 'job' not in chat_data:
-        update.message.reply_text('You have no active timer')
-        return
+    if check_if_bot_admin(update.message.from_user.id):
+        if 'job' not in chat_data:
+            update.message.reply_text('You have no active timer')
+            return
 
-    job = chat_data['job']
-    job.schedule_removal()
-    del chat_data['job']
+        job = chat_data['job']
+        job.schedule_removal()
+        del chat_data['job']
 
-    update.message.reply_text('Timer successfully unset!')
+        update.message.reply_text('Timer successfully unset!')
 
 
-def error(bot, update, error):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, error)
+    def error(bot, update, error):
+        """Log Errors caused by Updates."""
+        logger.warning('Update "%s" caused error "%s"', update, error)
 
 
 welcome_handler = MessageHandler(Filters.status_update, empty_message)
